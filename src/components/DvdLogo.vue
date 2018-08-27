d78e2e8477b2876a99fa4a7e6c633533237bde56
Eddy Lu <eddy.lu@mail.mcgill.ca>
2018-08-26 21:36:36 -0400

    Implement bouncing animation

--- src/components/DvdLogo.vue
+++ src/components/DvdLogo.vue
@@ -11,9 +11,44 @@ export default {
       dvdLogoStyle: {
         width: '150px',
         height: '100px',
-        backgroundColor: '#0000FF'
+        backgroundColor: '#0000FF',
+        transform: 'translate(0px, 0px)'
       }
     }
+  },
+  mounted () {
+    this.posX = 0
+    this.posY = 0
+    var changeXMag = 1
+    var changeYMag = 1
+    var changeX = changeXMag
+    var changeY = changeYMag
+    var distance = 0;
+
+    var interval = setInterval(() => {
+      this.posX += changeX
+      this.posY += changeY
+
+      var isRight = this.posX >= 800 - 150
+      var isLeft = this.posX <= 0
+      var isTop = this.posY <= 0
+      var isBottom = this.posY >= 400 - 100
+
+      var isHorizontalBoundary = isLeft || isRight
+      var isVerticalBounday = isTop || isBottom
+      
+      if (isHorizontalBoundary && isVerticalBounday) {
+        clearInterval(interval)
+      }
+      if (isHorizontalBoundary) {
+        changeX *= -1
+      }
+      if (isVerticalBounday) {
+        changeY *= -1
+      }
+      
+      this.dvdLogoStyle.transform = `translate(${this.posX}px, ${this.posY}px)`
+    }, 10)
   }
 }
 </script>
